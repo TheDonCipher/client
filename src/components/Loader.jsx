1226b50afaa3ead75ffefcc3d421727d2f61ea1c
TheDonCipher <eugeniusratshipa@gmail.com>
2023-03-29 05:11:13 +0200

    Added Laders

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import { loader } from '../assets';
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-flex">
+    <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-col">
       <img
         src={loader}
         alt="loader"
